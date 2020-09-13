e2b4f361ab06b3f7397747d2dff02cf30ef2afa4
Nguyen Quang Duy <quangduyntt2000@gmail.com>
2020-09-13 21:26:12 +0700

    Mortgage Calculation

--- JavaHomework/src/Mortgage.java
+++ JavaHomework/src/Mortgage.java
@@ -4,18 +4,18 @@ public class Mortgage {
     public static void main(String[] args) {
 
         try{
-            int P; //Principal
+            int p; //Principal
             double r; //rate
             int n; //Period
             Scanner value = new Scanner(System.in);
             System.out.println("Enter Principal:");
-            P = value.nextInt();
+            p = value.nextInt();
             System.out.println("Enter Rate:");
             r = value.nextDouble();
             System.out.println("Enter Period:");
             n = value.nextInt();
             double a = Math.pow((r + 1), n);
-            int M = (int)(P * r * (a / (a - 1)));
+            double M = (int)(p * r * (a / (a - 1)));
             System.out.println("Your mortgage payment is: " + M + "$" );
 
         }
