8d767960e94c810bb0c6aa038c343d7ec7572302
Nguyen Quang Duy <quangduyntt2000@gmail.com>
2020-09-13 20:07:46 +0700

    ConGiap.java "Nhập số ngẫu nhiên và in ra năm con gì"

--- Java 10-9/src/ConGiap.java
+++ Java 10-9/src/ConGiap.java
@@ -4,48 +4,22 @@ public class ConGiap {
     public static void main(String[] args) {
         int choice;
         Scanner input = new Scanner(System.in);
-        System.out.println("Nhập năm:");
+        System.out.println("Nhập số:");
         choice = input.nextInt();
-        switch (choice){
-            case 0:
-                System.out.println("Đây là năm Tí!");
-                break;
-            case 1:
-                System.out.println("Đây là năm Sửu!");
-                break;
-            case 2:
-                System.out.println("Đây là năm Dần!");
-                break;
-            case 3:
-                System.out.println("Đây là năm Mão!");
-                break;
-            case 4:
-                System.out.println("Đây là năm Thìn!");
-                break;
-            case 5:
-                System.out.println("Đây là năm Tỵ!");
-                break;
-            case 6:
-                System.out.println("Đây là năm Ngọ!");
-                break;
-            case 7:
-                System.out.println("Đây là năm Mùi!");
-                break;
-            case 8:
-                System.out.println("Đây là năm Thân!");
-                break;
-            case 9:
-                System.out.println("Đây là năm Dậu!");
-                break;
-            case 10:
-                System.out.println("Đây là năm Tuất!");
-                break;
-            case 11:
-                System.out.println("Đây là năm Hợi!");
-                break;
-            default:
-                System.out.println("Lựa chọn không nằm trong danh sách. Vui lòng thử lại!");
-
+        switch (choice) {
+            case 0 -> System.out.println("Đây là năm Tí!");
+            case 1 -> System.out.println("Đây là năm Sửu!");
+            case 2 -> System.out.println("Đây là năm Dần!");
+            case 3 -> System.out.println("Đây là năm Mão!");
+            case 4 -> System.out.println("Đây là năm Thìn!");
+            case 5 -> System.out.println("Đây là năm Tỵ!");
+            case 6 -> System.out.println("Đây là năm Ngọ!");
+            case 7 -> System.out.println("Đây là năm Mùi!");
+            case 8 -> System.out.println("Đây là năm Thân!");
+            case 9 -> System.out.println("Đây là năm Dậu!");
+            case 10 -> System.out.println("Đây là năm Tuất!");
+            case 11 -> System.out.println("Đây là năm Hợi!");
+            default -> System.out.println("Lựa chọn không nằm trong danh sách. Vui lòng thử lại!");
         }
     }
 }
