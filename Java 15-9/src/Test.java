cdbb9f1770b4f209e1ea7517bafcbf3beef1593c
Nguyen Quang Duy <quangduyntt2000@gmail.com>
2020-09-17 15:04:35 +0700

    Test ngày 15-9-2020

---
+++ Java 15-9/src/Test.java
@@ -0,0 +1,70 @@
+public class Test {
+    public static void main(String[] args) {
+        String s1 = "Welcome";
+        String s2 = "welcome";
+        //Câu a:
+        boolean isEqual = s1 == s2;
+        System.out.println(isEqual);
+
+        //Câu b:
+//        boolean isEqual = s1.equalsIgnoreCase(s2);
+//        System.out.println(isEqual);
+
+        //Câu c:
+//        int x = s1.compareTo(s2);
+//        System.out.println(x);
+
+        //Câu d:
+//        int x = s1.compareToIgnoreCase(s2);
+//        System.out.println(x);
+
+        //Câu e:
+//        boolean b = s2.startsWith("AAA");
+//        System.out.println(b);
+
+        //Câu f:
+//        boolean b = s2.endsWith("AAA");
+//        System.out.println(b);
+
+        //Câu g:
+//        int x = s1.length();
+//        System.out.println(x);
+
+        //Câu h:
+//        char x = s1.substring(0);
+
+        //Câu i:
+//        String s3 = s1 + s2;
+//        System.out.println(s3);
+
+        //Câu j:
+//        String x = s1.substring(1);
+//        System.out.println(x);
+
+        //Câu k:
+//        String y = s1.substring(1, 4);
+//        System.out.println(y);
+
+        //Câu l:
+//        String s3 = s1.toLowerCase();
+//        System.out.println(s3);
+
+        //Câu m:
+//        String s3 = s1.toUpperCase();
+//        System.out.println(s3);
+
+        //Câu n:
+//        String s3 = s1.trim();
+//        System.out.println(s3);
+
+        //Câu o:
+//        int x = s1.indexOf('e', 0);
+//        System.out.println(x);
+
+        //Câu p:
+//        int x = s1.lastIndexOf("abc");
+//        System.out.println(x);
+
+
+    }
+}
