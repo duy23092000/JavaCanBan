99667e7488da3d70e4801c96e706701ea7341c9c
Nguyen Quang Duy <quangduyntt2000@gmail.com>
2020-09-18 20:15:05 +0700

    Năm nhuận OOP

---
+++ Java 17-9/src/leapyear/Main.java
@@ -0,0 +1,14 @@
+package leapyear;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Nhập năm: ");
+        var year = new Input();
+        year.getYearInput();
+        var display = new Input();
+        display.display();
+    }
+
+
+
+}
