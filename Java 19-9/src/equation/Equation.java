5e6d80c0e6fb340a939b146c238dbb29f963ac67
Nguyen Quang Duy <quangduyntt2000@gmail.com>
2020-09-22 18:35:11 +0700

    Pt bậc 2

---
+++ Java 19-9/src/equation/Equation.java
@@ -0,0 +1,37 @@
+package equation;
+
+import java.util.Scanner;
+
+public class Equation {
+    private int a;
+    private int b;
+    private int c;
+
+
+    public void setValue() {
+        Scanner inputValue = new Scanner(System.in);
+        a = inputValue.nextInt();
+        b = inputValue.nextInt();
+        c = inputValue.nextInt();
+    }
+
+    public void getCalculation() {
+        double deltaValue = b * b - 4 * a * c;
+        if (deltaValue > 0 ){
+            int x1 = (int)(-b - Math.sqrt(deltaValue)) / (2 * a);
+            int x2 = (int)(-b + Math.sqrt(deltaValue)) / (2 * a);
+            System.out.println("Phương trình có 2 nghiệm phân biệt!");
+            System.out.println("Nghiệm thứ nhât là: " + x1);
+            System.out.println("Nghiệm thứ hai là: " + x2);
+        }
+        else if (deltaValue == 0) {
+            int x = (-b / (2 * a));
+            System.out.println("Phương trình có 1 nghiệm kép " + x);
+        }
+        else {
+            System.out.println("Phương trình vô nghiệm!");
+        }
+    }
+
+
+}
---
+++ Java 19-9/src/equation/Main.java
@@ -0,0 +1,12 @@
+package equation;
+
+public class Main {
+    public static void main(String[] args) {
+        var value1 = new Equation();
+        System.out.println("Nhập giá trị của a:");
+        System.out.println("Nhập giá trị của b:");
+        System.out.println("Nhập giá trị của c:");
+        value1.setValue();
+        value1.getCalculation();
+    }
+}
