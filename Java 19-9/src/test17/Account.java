0ac252072c061412149cd9ce9d644f1ab9d4e5e1
Nguyen Quang Duy <quangduyntt2000@gmail.com>
2020-09-22 09:54:57 +0700

    Account

---
+++ Java 19-9/src/test17/Account.java
@@ -0,0 +1,62 @@
+package test17;
+
+import java.util.Date;
+
+public class Account {
+    private int accountID;
+    private double accountBalance;
+    private double annualInterestRate;
+    private Date dateCreated;
+
+    public Account(){
+
+    }
+
+    public Account(int accountID, double accountBalance) {
+        this.accountID = accountID;
+        this.accountBalance = accountBalance;
+    }
+
+    public int getAccountID() {
+        return accountID;
+    }
+
+    public void setAccountID(int accountID) {
+        this.accountID = accountID;
+    }
+
+    public double getAccountBalance() {
+        return accountBalance;
+    }
+
+    public void setAccountBalance(double accountBalance) {
+        this.accountBalance = accountBalance;
+    }
+
+    public double getAnnualInterestRate() {
+        return annualInterestRate;
+    }
+
+    public void setAnnualInterestRate(double annualInterestRate) {
+        this.annualInterestRate = annualInterestRate;
+    }
+
+    public double getMonthlyInterestRate() {
+        return (annualInterestRate / 12) * 100;
+    }
+
+    public void deposit (double depositAmount) {
+        accountBalance += depositAmount;
+    }
+
+    public void withdraw (double withdrawAmount) {
+        if (withdrawAmount <= accountBalance){
+            accountBalance -= withdrawAmount;
+        }
+        else{
+            System.out.println("Số dư không đủ!");
+        }
+    }
+
+
+}
